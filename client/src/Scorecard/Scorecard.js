3577d0828b5b38c355aab0d80e1a8cf0e83ac113
mdb1710 <mdb1710@gmail.com>
2019-10-23 18:20:54 -0400

    changes to homepage and quizsection

--- client/src/Scorecard/Scorecard.js
+++ client/src/Scorecard/Scorecard.js
@@ -1,16 +1,14 @@
-import React from 'react';
-import Timer from '../Timer/Timer';
+import React from "react";
+import Timer from "../Timer/Timer";
 
 const Scorecard = () => {
-
-    return(
-        <div className='scorecard'>
-        
-          <p>There are 21 questons</p>
-          <p>Answer all before time runs OUT!</p>
-          <Timer />
-        </div>
-    )
-}
+  return (
+    <div className="scorecard">
+      <p>There are 8 questons</p>
+      <p>Please answer yes or no for each question</p>
+      <Timer />
+    </div>
+  );
+};
 
 export default Scorecard;
